e2d8b4cde9d8b820b9fa9e3a82f1b9bbea7ddbe2
Andre Gebel <andre.gebel97@outlook.de>
2025-02-26 22:39:44 +0100

    remove sudo

--- samples/ubuntu/ubuntu-noble-cloudinit.sh
+++ samples/ubuntu/ubuntu-noble-cloudinit.sh
@@ -7,20 +7,20 @@ set -x
 rm -f noble-server-cloudimg-amd64.img
 wget -q https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
 qemu-img resize noble-server-cloudimg-amd64.img 32G
-sudo qm destroy $VMID
-sudo qm create $VMID --name "ubuntu-24.04.LTS-template" --ostype l26 \
+ qm destroy $VMID
+ qm create $VMID --name "ubuntu-24.04.LTS-template" --ostype l26 \
     --memory 1024 --balloon 0 \
     --agent 1 \
     --bios ovmf --machine q35 --efidisk0 $STORAGE:0,pre-enrolled-keys=0 \
     --cpu host --socket 1 --cores 1 \
     --vga serial0 --serial0 socket  \
     --net0 virtio,bridge=vmbr0
-sudo qm importdisk $VMID noble-server-cloudimg-amd64.img $STORAGE
-sudo qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
-sudo qm set $VMID --boot order=virtio0
-sudo qm set $VMID --scsi1 $STORAGE:cloudinit
+ qm importdisk $VMID noble-server-cloudimg-amd64.img $STORAGE
+ qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
+ qm set $VMID --boot order=virtio0
+ qm set $VMID --scsi1 $STORAGE:cloudinit
 
-cat << EOF | sudo tee /var/lib/vz/snippets/ubuntu.yaml
+cat << EOF |  tee /var/lib/vz/snippets/ubuntu.yaml
 #cloud-config
 runcmd:
     - apt-get update
@@ -30,9 +30,9 @@ runcmd:
 # Taken from https://forum.proxmox.com/threads/combining-custom-cloud-init-with-auto-generated.59008/page-3#post-428772
 EOF
 
-sudo qm set $VMID --cicustom "vendor=local:snippets/ubuntu.yaml"
-sudo qm set $VMID --tags ubuntu-template,noble,cloudinit,24.04.LTS
-sudo qm set $VMID --ciuser $USER
-sudo qm set $VMID --sshkeys ~/.ssh/authorized_keys
-sudo qm set $VMID --ipconfig0 ip=dhcp
-sudo qm template $VMID
+ qm set $VMID --cicustom "vendor=local:snippets/ubuntu.yaml"
+ qm set $VMID --tags ubuntu-template,noble,cloudinit,24.04.LTS
+ qm set $VMID --ciuser $USER
+ qm set $VMID --sshkeys ~/.ssh/authorized_keys
+ qm set $VMID --ipconfig0 ip=dhcp
+ qm template $VMID
